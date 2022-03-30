dc2034c0f57a1270bbcb58e6773125b78fe43e1d
siriuska <you@example.com>
2022-03-30 18:49:47 +0200

    Added second index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  // Only change code below this line
+  /* I want to break free
+  I want to break free from your lies 🦾*/
+  // One liner comment
+  // Only change code above this line
+}
+console.log(main());
+module.exports = main;
